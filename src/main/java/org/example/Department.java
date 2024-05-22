22cb9a64f02c764c51089b7d02a9e1fb732e9d52
Loading... <helene.m.rousseau@gmail.com>
2024-05-22 00:55:31 -0400

    Department
    Creation of the class <Department> & its to do list

---
+++ src/main/java/org/example/Department.java
@@ -0,0 +1,22 @@
+package org.example;
+/*
+- Fields
+
+1. `String departmentId` // 2-digits starts with a character `D`
+2. `String departmentName`
+3. `static int nextId` // indicates the next ID that will be used
+
+- Methods
+
+1. `static boolean validateDepartmentName(String departmentName)` // checks if a department name is valid or not, a
+department name should only contain letters or space
+2. Constructor // if the `departmentName` is invalid, create the object with everything as `null`;
+3. toString
+4. equals
+5. getter
+6. setter
+ */
+public class Department {
+
+
+}
