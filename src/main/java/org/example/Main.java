4212207236464133d517422e3ee7528eebf73e76
Loading... <helene.m.rousseau@gmail.com>
2024-05-22 01:05:04 -0400

    Test To Do
    creation of the to do list of the unit testing in the main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,19 @@
 package org.example;
+/*
+## Unit Testing
 
+The following methods requires unit testing:
+
+1. `Address`
+   1. `isPostalCodeValid`
+2. `Assignment`
+   1. `calcAssignmentAvg` // you will need to create object of an assignment, then use setter to change the scores of it, instead of using the random generated scores.
+   2. `isAssignmentsTotalWeightValid`
+3. `Course`
+   1. `calcStudentAvg`
+4. `Util`
+   1. `toTitleCase`
+ */
 public class Main {
     public static void main(String[] args) {
 
