262744fb5f5e3601f110467320ed07917079b007
Loading... <helene.m.rousseau@gmail.com>
2024-05-22 01:04:02 -0400

    Util
    Creation of the class <Util> & its to do list

---
+++ src/main/java/org/example/Util.java
@@ -0,0 +1,14 @@
+package org.example;
+/*
+A class `Util` locates in a new package `util` which contains
+- Methods:
+
+1. `static String toTitleCase(String strIn)` // converts each word in a string to title case,
+such as `Yi Wang`, `Computer Science`. You can
+   1. simply assume the string `strIn` only contains two words with one space in between,
+   2. or you can implement the method for general situation, in this case, `str.split()` method will be used.
+   3. All `studentName`, `departmentName`, `courseName` needs to be in title case.
+ */
+public class Util {
+
+}
