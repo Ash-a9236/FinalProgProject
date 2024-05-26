1a8975f5c4cf9b9feb8845a6f9d0cc070c900e14
Loading... <helene.m.rousseau@gmail.com>
2024-05-26 14:17:41 -0400

    Assignment > generateRandomScore + toString

--- src/main/java/org/example/Assignment.java
+++ src/main/java/org/example/Assignment.java
@@ -28,6 +28,8 @@ generated with the following rule: Firstly generate a random number in range `[0
 import lombok.Getter;
 
 import java.util.ArrayList;
+import java.util.Random;
+
 public class Assignment {
     private String assignmentId;
     @Getter
@@ -57,5 +59,31 @@ public class Assignment {
             sum += score;
         }
         System.out.println(sum/scores.size());
+    }
+
+    public void generateRandomScore() {
+        Random rand = new Random();
+        int randomNumber = rand.nextInt(0,11);
+
+        if (randomNumber == 0) {
+            randomNumber = rand.nextInt(0,60);
+        } else if (randomNumber == 1 || randomNumber == 2) {
+            randomNumber = rand.nextInt(60,70);
+        } else if (randomNumber == 3 || randomNumber == 4) {
+            randomNumber = rand.nextInt(70,80);
+        } else if (randomNumber == 5 || randomNumber == 6 || randomNumber == 7 || randomNumber == 8) {
+            randomNumber = rand.nextInt(80,90);
+        } else if (randomNumber == 9 || randomNumber == 10) {
+            randomNumber = rand.nextInt(90,101);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Assignment : " +
+                "\nAssignment Id : " + assignmentId +
+                "\nAssignment Name : " + assignmentName +
+                "\nWeight : " + weight +
+                "\nMax Score : " + maxScore);
     }
 }
