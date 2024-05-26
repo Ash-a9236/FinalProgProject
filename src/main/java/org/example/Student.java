a55d3ad458c9ddfb602966d33fdcb0b82c151ea6
Loading... <helene.m.rousseau@gmail.com>
2024-05-26 13:29:00 -0400

    Update Student " Fields
    all the fields in the student class

--- src/main/java/org/example/Student.java
+++ src/main/java/org/example/Student.java
@@ -37,4 +37,11 @@ the `gender`, the `address` and the `department`, and the `registeredCourses`
 8. setters
  */
 public class Student {
+    private String studentId;
+    private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private Course[] registeredCourses;
+    private static int nextId;
 }
