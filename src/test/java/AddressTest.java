7da57c9eeb46aaef207704ed0a4a24ccbf47d767
Loading... <helene.m.rousseau@gmail.com>
2024-05-26 20:27:30 -0400

    Address Test

--- src/test/java/AddressTest.java
+++ src/test/java/AddressTest.java
@@ -1,9 +1,89 @@
+import org.example.Address;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class AddressTest {
+    @Test
+    public void testIsPostalCodeValid1() {
+        String str = "H3X3R7";
 
-    /*
-    The following methods requires unit testing:
+        Assertions.assertTrue(Address.isPostalCodeValid(str));
+    }
 
-1. `Address`
-   1. `isPostalCodeValid`
-     */
+    @Test
+    public void testIsPostalCodeValid2() {
+        String str = "H3X 3R7";
+
+        Assertions.assertTrue(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid3() {
+        String str = "h3x3r7";
+
+        Assertions.assertTrue(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid4() {
+        String str = "h3x 3r7";
+
+        Assertions.assertTrue(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid5() {
+        String str = "null";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid6() {
+        String str = "";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid7() {
+        String str = "H3X=3R7";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid8() {
+        String str = "h3x=3r7";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid9() {
+        String str = "H3X3R";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid10() {
+        String str = "H3X 3R";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid11() {
+        String str = "123456";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
+
+    @Test
+    public void testIsPostalCodeValid12() {
+        String str = "abcdef";
+
+        Assertions.assertFalse(Address.isPostalCodeValid(str));
+    }
 }
